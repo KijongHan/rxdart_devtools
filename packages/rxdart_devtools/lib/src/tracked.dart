92b6ea373d73864dbaec61ef5dd85397929eeb2c
Thomas Han <thomas.kijong.han@gmail.com>
2026-06-04 18:55:58 +1200

    clean up tracked extension method types

--- packages/rxdart_devtools/lib/src/tracked.dart
+++ packages/rxdart_devtools/lib/src/tracked.dart
@@ -2,13 +2,10 @@ import 'package:flutter/foundation.dart';
 
 import 'registry.dart';
 
-extension RxDartDevtoolsTracking<T> on Stream<T> {
-  Stream<T> tracked(String? name, {int? historySize}) {
+extension RxDartDevtoolsTracking<T, S extends Stream<T>> on S {
+  S tracked(String? name, {int? historySize}) {
     if (kReleaseMode) return this;
-    return _trackImpl(name, historySize);
-  }
 
-  Stream<T> _trackImpl(String? name, int? historySize) {
     Registry.instance.register<T>(this, name: name, historySize: historySize);
     return this;
   }
